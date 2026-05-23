b6d726387bbe2bde030ce39568c1312ea31ae7d5
Mudriy Daniil <mudry.daniil@gmail.com>
2026-05-23 14:58:13 +0300

    feat: add movie search on homepage

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -1,10 +1,13 @@
-export const baseUrl = 'https://kinopoiskapiunofficial.tech/api/v2.2/'
+export const baseUrl = 'https://kinopoiskapiunofficial.tech/api/' // Убрали v2.2 отсюда
 
 export const API = {
     MOVIES: {
-        LIST: 'films/collections?type=TOP_POPULAR_ALL', 
-        DETAILS: (id: number | string) => `films/${id}`,
-        SEARCH: 'films/search-by-keyword/',
+        // Обычная коллекция живет в v2.2
+        LIST: 'v2.2/films/collections?type=TOP_POPULAR_ALL', 
+        DETAILS: (id: number | string) => `v2.2/films/${id}`,
+        // Поиск по ключевому слову живет СТРОГО в v2.1
+        SEARCH: (keyword: string, page: number) => 
+            `v2.1/films/search-by-keyword?keyword=${encodeURIComponent(keyword)}&page=${page}`,
     },
-    FILTERS: 'films/filters',
+    FILTERS: 'v2.2/films/filters',
 } as const;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,27 @@
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchMovies } from '../../redux/slices/catalog-slice'
+import { fetchMovies, searchMovies } from '../../redux/slices/catalog-slice'
 import { AppDispatch, RootState } from '../../redux/store'
 import { MovieCard } from '../../components/MovieCard/MovieCard'
 
 export const HomePage = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const { movies, isLoading, error } = useSelector((state: RootState) => state.catalog);
+  const { movies, total, isLoading, error, searchQuery } = useSelector((state: RootState) => state.catalog);
   const [currentPage, setCurrentPage] = useState(1);
 
+  // При изменении поисковой строки всегда сбрасываем пагинацию на первую страницу
   useEffect(() => {
-    dispatch(fetchMovies(currentPage));
-  }, [dispatch, currentPage]);
+    setCurrentPage(1);
+  }, [searchQuery]);
+
+  // Запрос данных в зависимости от режима (поиск или каталог)
+  useEffect(() => {
+    if (searchQuery.trim()) {
+      dispatch(searchMovies({ keyword: searchQuery, page: currentPage }));
+    } else {
+      dispatch(fetchMovies(currentPage));
+    }
+  }, [dispatch, currentPage, searchQuery]);
 
   const handleShowMore = () => {
     setCurrentPage(prev => prev + 1);
@@ -19,15 +29,23 @@ export const HomePage = () => {
 
   return (
     <div className="home-page">
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {error && <p style={{ color: 'red', textAlign: 'center' }}>{error}</p>}
       
+      {!isLoading && movies.length === 0 && searchQuery && (
+        <p style={{ color: '#fff', textAlign: 'center', marginTop: '40px' }}>
+          No movies found for "{searchQuery}"
+        </p>
+      )}
+
       <div className="movie-grid">
-        {movies.map((movie) => (
-          <MovieCard key={movie.kinopoiskId} movie={movie} />
+        {movies.map((movie, index) => (
+          // Ошибка типизации исправлена: filmId удален, данные нормализованы в Redux
+          <MovieCard key={`${movie.kinopoiskId}-${index}`} movie={movie} />
         ))}
       </div>
 
-      {!isLoading && movies.length > 0 && (
+      {/* Кнопка показывается только если загружены не все доступные элементы */}
+      {!isLoading && movies.length > 0 && movies.length < total && (
         <div className="pagination-wrapper">
           <button className="show-more" onClick={handleShowMore}>
             Show more
@@ -35,7 +53,7 @@ export const HomePage = () => {
         </div>
       )}
 
-      {isLoading && <p style={{ textAlign: 'center', color: '#fff' }}>Loading...</p>}
+      {isLoading && <p style={{ textAlign: 'center', color: '#fff', marginTop: '20px' }}>Loading...</p>}
     </div>
   );
 };

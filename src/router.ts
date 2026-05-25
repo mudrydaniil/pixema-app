4fd0eb3bcd7f56050080666226c1de49e2920fc9
Mudriy Daniil <mudry.daniil@gmail.com>
2026-05-25 17:00:28 +0300

    chore: refactoring code

--- src/router.ts
+++ src/router.ts
@@ -28,7 +28,7 @@ export const router = createBrowserRouter([
       },
       {
         path: 'settings',
-        element: 'Настройки (Заглушка)'
+        element: 'Настройки'
       },
       {
         path: '*',
